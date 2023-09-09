dcf6102bb0fa91517d874d054e5955cabe92477d
Samya brata Maji <samyabratamaji334@gmail.com>
2023-09-09 19:18:24 +0530

    refactor: minor changes

--- app/(main)/(static)/page.tsx
+++ app/(main)/(static)/page.tsx
@@ -7,21 +7,23 @@ import ImgDashboard from "@/public/assets/img/intellij.png";
 import { PricingSection } from "@/components/pricing-card";
 import FooterSection from "@/components/footer-section";
 import FaqSection from "@/components/faq-section";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+	const router = useRouter()
 	return (
 		<>
-			<main className="flex flex-col min-h-[calc(100vh-56px)] gap-8 p-24 items-center justify-center">
+			<main className="flex flex-col min-h-[calc(100vh-56px)] gap-8 p-24 max-sm:px-8 max-sm:py-16 items-center justify-center">
 				<TypographyH1 className="text-8xl lg:text-7xl">
 					Launch in Seconds
 				</TypographyH1>
-				<TypographyP className="text-2xl w-2/3 text-center">
+				<TypographyP className="text-2xl w-2/3 max-sm:w-full text-center">
 					A simple customizable starter kit to quickly bootstrap a
 					Next.js project.
 				</TypographyP>
-				<div className="space-x-4">
-					<Button size="lg">Get Started</Button>
-					<Button size="lg" variant="outline">
+				<div className="space-x-4 max-sm:space-y-4 max-sm:space-x-0">
+					<Button size="lg" className="max-sm:w-[100%]" onClick={() => router.push("/dashboard")}>Get Started</Button>
+					<Button size="lg" variant="outline" className="max-sm:w-[100%]">
 						Read Docs
 					</Button>
 				</div>
--- components/pricing-card/pricing-section.tsx
+++ components/pricing-card/pricing-section.tsx
@@ -41,8 +41,8 @@ export const pricing_perks: PricingOptions[] = [
 
 export default function PricingSection() {
 	return (
-		<section className="grid grid-rows-[auto_1fr] grid-cols-3 max-md:grid-cols-2 max-sm:grid-cols-1 gap-8 pt-8 pb-16 px-24">
-			<div className="col-span-3 max-md:col-span-2 max-sm:col-span-1 w-2/3 flex flex-col items-center justify-center justify-self-center">
+		<section className="grid grid-rows-[auto_1fr] grid-cols-3 max-md:grid-cols-2 max-sm:grid-cols-1 gap-8 pt-8 pb-16 px-24 max-sm:px-8">
+			<div className="col-span-3 max-md:col-span-2 max-sm:col-span-1 w-2/3 max-sm:w-full flex flex-col items-center justify-center justify-self-center">
 				<TypographyH2 className="border-none font-extrabold">
 					Why Next Starter?
 				</TypographyH2>
