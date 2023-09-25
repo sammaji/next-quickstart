5fa6526c3c37371e9ee0b89ad6cdcb9afc3fde0c
samyabrata-maji <samyabratamaji334@gmail.com>
2023-09-26 02:46:17 +0530

    feat: improved shell script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,23 +1,4 @@
 #! /usr/bin/env sh
-
-function prompt_install_depp() {
-    while true; do
-        read -p "Do you want to install dependencies? (y/N): " choice
-        case "$choice" in
-            [yY])
-                npm i
-                break
-                ;;
-            [nN])
-                break
-                ;;
-            *)
-                echo "Invalid choice."
-                ;;
-        esac
-    done
-}
-
 read -p "What your project name? (next-starter) " name
 if [ -z "$name" ]; then
     name="next-starter"
@@ -31,6 +12,22 @@ rm package-lock.json
 sed "s/next-starter/$name/g" package.json > tempfile
 cat tempfile > package.json
 rm tempfile
+
+while true; do
+    read -p "Do you want to install dependencies? (y/N): " choice
+    case "$choice" in
+        [yY])
+            npm i
+            break
+            ;;
+        [nN])
+            break
+            ;;
+        *)
+            echo "Invalid choice."
+            ;;
+    esac
+done
 
 prompt_install_depp
 rm -rf ./scripts
